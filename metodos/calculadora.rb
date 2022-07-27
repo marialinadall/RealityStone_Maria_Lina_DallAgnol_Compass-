64e8b020389a04e251abb5de0be3259608c9fd43
Maria Lina <marialinacdallagnol@gmail.com>
2022-07-27 01:38:56 -0300

    cenários com a operação: subtração

--- metodos/calculadora.rb
+++ metodos/calculadora.rb
@@ -2,4 +2,8 @@ class Calculadora
     def soma (x,y)
         soma = x + y
     end 
+
+    def subtrai (x,y)
+        subtração = x - y
+    end 
 end 
--- specs/calculadora_spec.rb
+++ specs/calculadora_spec.rb
@@ -19,6 +19,25 @@ describe('Calculadora') do
     end 
 
     it('Deve somar com o número 0') do 
-        expect(calculadora.soma(0,8)).to eq 8
+        expect(calculadora.soma(0,4)).to eq 4
     end 
+
+    #operação: subtração
+
+    it('Deve subtrair dois números positivos') do
+        expect(calculadora.subtrai(20,12)).to eq 8
+    end 
+
+    it('Deve subtrair dois números negativos') do
+        expect(calculadora.subtrai(-20,-12)).to eq -8
+    end 
+
+    it('Deve subtrair um número positivo e outro negativo') do
+        expect(calculadora.subtrai(20,-12)).to eq 32
+    end
+
+    it('Deve subtrair se conter 0 na equação') do
+        expect(calculadora.subtrai(0,-12)).to eq 12
+    end 
+
 end
